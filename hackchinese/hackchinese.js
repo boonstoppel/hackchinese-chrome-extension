f5dfb960810fab27527b59577d775babaaceeeea
Tobias Boonstoppel <boonstoppel@gmail.com>
2021-04-26 17:58:58 -0400

    fix animation

--- hackchinese/hackchinese.js
+++ hackchinese/hackchinese.js
@@ -1,101 +1,135 @@
 var animationIsOn = false;
 var charactersByCard = [];
+var highlightRadicalsLoopCounter = [];
 
-function startAnimation(characters) {
-  animationIsOn = true;
+function initSession() {
+  var cardId = $('.card:visible').attr('data-card');
+  highlightRadicalsLoop(cardId);
+}
 
-  for (var i = 0; i < characters.length; i++) {
-    characters[i].hideCharacter();
-    characters[i].updateColor('radicalColor', '#000000');
+function initCard() {
+  var cardId = $(this).attr('data-card');
+  charactersByCard[cardId] = [];
+  highlightRadicalsLoopCounter[cardId] = 0;
+
+  $(this).find('.dictionaryHanzi').each(function(index) {
+    initCharacter(index, this, cardId);
+  });
+}
+
+function initCharacter(index, hanziCharacter, cardId) {
+  var character = $(hanziCharacter).html();
+
+  if (character == '…') {
+    return;
   }
 
-  characters[0].animateCharacter({
-    'onComplete': nextAnimation.bind(this, 0, characters)
+  var elemId = 'hanzi-character-' + cardId + '-' + index;
+
+  $(hanziCharacter).html('').attr('id', elemId);
+
+  charactersByCard[cardId].push(HanziWriter.create(elemId, character, {
+    'width': 80,
+    'height': 80,
+    'padding': 0,
+    'delayBetweenStrokes': 300,
+    'strokeColor': '#000000',
+    'radicalColor': '#000000',
+    'outlineColor': '#D3D3D3'
+  }));
+}
+
+function startAnimation(cardId) {
+  animationIsOn = true;
+
+  for (var i = 0; i < charactersByCard[cardId].length; i++) {
+    charactersByCard[cardId][i].hideCharacter();
+    charactersByCard[cardId][i].updateColor('radicalColor', '#000000');
+  }
+
+  charactersByCard[cardId][0].animateCharacter({
+    'onComplete': nextAnimation.bind(this, 0, cardId)
   })
 }
 
-function nextAnimation(index, characters) {
+function nextAnimation(index, cardId) {
   index++
 
-  if (!characters[index]) {
-    highlightRadicals(characters);
-    animationIsOn = false;
+  if (!charactersByCard[cardId][index]) {
+    highlightRadicals(cardId);
     
     setTimeout(function() {
-      highlightRadicalsLoop(characters)
+      animationIsOn = false;
+      highlightRadicalsLoopCounter[cardId] = 0;
+      highlightRadicalsLoop(cardId);
     }, 2000);
   } else {
     setTimeout(function() {
-      characters[index].animateCharacter({
-        'onComplete': nextAnimation.bind(this, index, characters)
+      charactersByCard[cardId][index].animateCharacter({
+        'onComplete': nextAnimation.bind(this, index, cardId)
       })
-    }, 1000);
+    }, 600);
   }
 }
 
-function setRadicalColor(characters, color) {
-  for (var i = 0; i < characters.length; i++) {
-    characters[i].updateColor('radicalColor', color);
+function setRadicalColor(cardId, color) {
+  for (var i = 0; i < charactersByCard[cardId].length; i++) {
+    charactersByCard[cardId][i].updateColor('radicalColor', color);
   }
 }
 
-function highlightRadicalsLoop(characters) {
+function highlightRadicalsLoop(cardId) {
   setTimeout(function() {
     if (!animationIsOn) {
-      setRadicalColor(characters, '#FF0000');
+      setRadicalColor(cardId, '#FF0000');
 
       setTimeout(function() {
         if (!animationIsOn) {
-          setRadicalColor(characters, '#000000');
+          setRadicalColor(cardId, '#000000');
 
           setTimeout(function() {
-            highlightRadicalsLoop(characters)
-          }, 2000);
+            if (animationIsOn) {
+              return;
+            }
+
+            if (highlightRadicalsLoopCounter[cardId] < 6) {
+              highlightRadicalsLoopCounter[cardId]++;
+              highlightRadicalsLoop(cardId)
+            } else {
+              highlightRadicalsLoopCounter[cardId] = 0;
+            }
+          }, 2600);
         }
       }, 1000);
     }
   }, 2000);
 }
 
-function highlightRadicals(characters) {
+function highlightRadicals(cardId) {
   setTimeout(function() {
-    setRadicalColor(characters, '#FF0000');
+    setRadicalColor(cardId, '#FF0000');
 
     setTimeout(function() {
-      setRadicalColor(characters, '#000000');
+      setRadicalColor(cardId, '#000000');
     }, 1000);
   }, 500);
 }
 
-$(document).keydown(function(event) {
+function keyHandler(event) {
+  var cardId = $('.card:visible').attr('data-card');
+
   if (event.which == 87) {
-    var cardId = $('.card:visible').attr('data-card');
-    startAnimation(charactersByCard[cardId]);
+    startAnimation(cardId);
   }
-});
 
+  if (event.which == 40 || event.which == 37 || event.which == 39) {
+    if (highlightRadicalsLoopCounter[cardId] == 0) {
+      highlightRadicalsLoop(cardId);
+    }
+  }
+}
 
-$('.card').each(function() {
-  var cardId = $(this).attr('data-card');
-  charactersByCard[cardId] = [];
+$(document).keydown(keyHandler);
+$('.card').each(initCard);
 
-  $(this).find('.dictionaryHanzi').each(function(index) {
-    var character = $(this).html();
-    var elemId = 'hanzi-character-' + cardId + '-' + index;
-
-    $(this).html('').attr('id', elemId);
-
-    charactersByCard[cardId].push(HanziWriter.create(elemId, character, {
-      'width': 80,
-      'height': 80,
-      'padding': 0,
-      'delayBetweenStrokes': 300,
-      'strokeColor': '#000000',
-      'radicalColor': '#000000',
-      'outlineColor': '#D3D3D3'
-    }));
-  });
-
-
-  highlightRadicalsLoop(charactersByCard[cardId]);
-});
+setTimeout(initSession, 1000);
