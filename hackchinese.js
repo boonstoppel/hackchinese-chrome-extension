bf77e95cec7d0b8a3b579c80f9a4968bd3cc9f53
Tobias Boonstoppel <boonstoppel@gmail.com>
2023-05-09 21:37:15 -0400

    fix

--- hackchinese.js
+++ hackchinese.js
@@ -1,27 +1,27 @@
-const translationUrl = 'https://helloacm.com/api/pinyin/?cached&t=1&s=';
+const translationUrl = 'https://helloacm.com/api/pinyin/?cached&t=1&s='
 
 const addPinyinKeyListener = () => {
 	document.addEventListener('keypress', (event) => {
-	 	if ( event.code == 'KeyP') {
-	 		togglePinyin();
+	 	if (event.code == 'KeyP') {
+	 		togglePinyin()
 	 	}
-	}, false);
+	}, false)
 }
 
 const stripHtml = (str) => {
-	return str.replace(/<\/?[^>]+(>|$)/g, '');
+	return str.replace(/<\/?[^>]+(>|$)/g, '')
 }
 
 const writeHanzi = () => {
-	var chars = document.getElementsByClassName('ss-character');
+	var chars = document.getElementsByClassName('ss-character')
 
 	if (!chars.length || chars[0].innerHTML.length != 1) {
-		return;
+		return
 	}
 
 	for (let i = 0; i < chars.length; i++) {
-		let char = chars[i].innerHTML;
-		chars[i].innerHTML = '';
+		let char = chars[i].innerHTML
+		chars[i].innerHTML = ''
 
 		HanziWriter.create(chars[i], char, {
 			 width: 80,
@@ -29,17 +29,17 @@ const writeHanzi = () => {
 			 padding: 0,
 			 radicalColor: '#024b30',
 			 strokeColor: '#000'
-		});
+		})
 	}
 }
 
 const parsePinyinData = (data) => {
-	var newData = [];
+	var newData = []
 
 	for (let i = 0; i < data.length; i++) {
 		newData.push(pinyinify(data[i])
 			.replace(/ /g, '')
-			.replace(/，/g, ','));
+			.replace(/，/g, ','))
 	}
 
 	return newData.join(' ')
@@ -49,68 +49,92 @@ const parsePinyinData = (data) => {
 }
 
 const togglePinyin = () => {
-	var elem = document.getElementsByClassName('ss-sentence-character')[0];
+	var elem = document.getElementsByClassName('ss-sentence-character')[0]
 	
 	if (!elem) {
-		return;
+		return
 	}
 
 	if (elem.hasPinyin) {
-		revertPinyin(elem);
+		revertPinyin(elem)
 	} else {
-		writePinyin(elem);
+		writePinyin(elem)
 	}
 }
 
+const playAudio = () => {
+	var elem = document.getElementsByClassName('ss-pinyin')[0]
+	
+	if (!elem) {
+		return
+	}
+
+	document.getElementById('replay-buttons').getElementsByClassName('cursor-pointer')[0].click()
+}
+
 const revertPinyin = (elem) => {
-	elem.innerHTML = elem.oldData;
-	elem.hasPinyin = false;
-	elem.oldData = null;
+	elem.innerHTML = elem.oldData
+	elem.hasPinyin = false
+	elem.oldData = null
 }
 
 const writePinyin = (elem) => {
-	elem.oldData = elem.innerHTML;
+	elem.oldData = elem.innerHTML
 
 	httpGet(translationUrl + stripHtml(elem.innerHTML), (data) => {
-		elem.innerHTML = parsePinyinData(data);
-	});
+		elem.innerHTML = parsePinyinData(data)
+	})
 
-	elem.hasPinyin = true;
+	elem.hasPinyin = true
 }
 
 const httpGet = (url, callback) => {
 	fetch(url, { method: 'GET' })
 		.then(Result => Result.json())
 	    .then(data => {
-			callback(data['result']);
+			callback(data['result'])
 	    })
 	    .catch(errorMsg => {
-	    	console.log(errorMsg);
-	    });
+	    	console.log(errorMsg)
+	    })
 }
 
 
 const setupObserver = () => {
-	const observer = new MutationObserver((mutationsList) => {
+	const characterObserver = new MutationObserver((mutationsList) => {
 	    for (var mutation of mutationsList) {
 	    	if (mutation.type === 'childList') {
-	        	writeHanzi();
+	        	writeHanzi()
 	    	}
 	    }
-	});
+	})
 
-	const nodes = document.getElementById('face-container').getElementsByClassName('ss-character');
-
-	for (let i = 0; i < nodes.length; i++) {
-		observer.observe(nodes[i], {
+	const characterNodes = document.getElementById('face-container').getElementsByClassName('ss-character')
+	for (let i = 0; i < characterNodes.length; i++) {
+		characterObserver.observe(characterNodes[i], {
 			childList: true
-		});
+		})
+	}
+
+	const pinyinObserver = new MutationObserver((mutationsList) => {
+		let elem = document.getElementById('face-container').getElementsByClassName('ss-pinyin')[0]
+
+		if (elem && elem.classList.contains('opacity-100')) {
+			playAudio()
+		}
+	})
+
+	const pinyinNodes = document.getElementById('face-container').getElementsByClassName('ss-pinyin')
+	for (let i = 0; i < pinyinNodes.length; i++) {
+		pinyinObserver.observe(pinyinNodes[i], {
+			attributes: true
+		})
 	}
 }
 
 
 setTimeout(() => {
-	setupObserver();
-	writeHanzi();
-	addPinyinKeyListener();
+	setupObserver()
+	writeHanzi()
+	addPinyinKeyListener()
 }, 100)
